20334df7f5564d6fec6a082a03fb932cfbf02781
Ray Andrew <ray.hack.azer@gmail.com>
2017-03-13 18:43:05 +0700

    Add ConfigStore and UTIL for parser and saver

--- src/class/ConfigStore/ConfigStore.cpp
+++ src/class/ConfigStore/ConfigStore.cpp
@@ -1,11 +1,5 @@
 #include "ConfigStore.h"
 
-void removeCharsFromString( string &str, char* charsToRemove ) {
-   for ( unsigned int i = 0; i < strlen(charsToRemove); ++i ) {
-      str.erase( remove(str.begin(), str.end(), charsToRemove[i]), str.end() );
-   }
-}
-
 int ConfigStore::parseFile(std::ifstream& inStream) {
     // Always check after opening to make sure the
     // file existed and was opened successfully.
@@ -21,8 +15,11 @@ int ConfigStore::parseFile(std::ifstream& inStream) {
     // clear to the readers.
     //
     const string ANIMAL = "[Animal]";
+    const string ENDANIMAL = "[END OF Animal]";
     const string CAGE = "[Cage]";
+    const string ENDCAGE = "[END OF Cage]";
     const string FACILITY = "[Facility]";
+    const string ENDFACILITY = "[END OF Facility]";
 
     vector<string> cage_str;
     vector<string> animal_str;
@@ -31,49 +28,67 @@ int ConfigStore::parseFile(std::ifstream& inStream) {
     //
     string lines;
     string lineCage;
-    string CageSeparator;
     while (std::getline(inStream, lines))
     {   
-        if (lines.empty()) continue;
-        if (lines.find(CAGE) != std::string::npos)
-        {
-            while (std::getline(inStream, lineCage) && !(lineCage.find(ANIMAL) != std::string::npos)) {
-               removeCharsFromString(lineCage, "{}()");
-               cage_str.push_back(lineCage);
+        if (!inStream.eof()) {
+            if (lines.find(CAGE) != std::string::npos)
+            {
+                while (std::getline(inStream, lineCage) && !(lineCage.find(ENDCAGE) != std::string::npos)) {   
+                if (lineCage.empty()) continue;
+                else cage_str.push_back(lineCage);
+                }
             }
-        }
-        if (lines.find(ANIMAL) != std::string::npos) {
-            while (std::getline(inStream, lineCage) && !(lineCage.find(FACILITY) != std::string::npos)) {
-               animal_str.push_back(lineCage);
+            if (lines.find(ANIMAL) != std::string::npos) {
+                while (std::getline(inStream, lineCage) && !(lineCage.find(ENDANIMAL) != std::string::npos)) {
+                if (lineCage.empty()) continue;
+                else animal_str.push_back(lineCage);
+                }
             }
-        }
-        if (lines.find(FACILITY) != std::string::npos) {
-            while (std::getline(inStream, lineCage)) {
-               facility_str.push_back(lineCage);
-            }
+            if (lines.find(FACILITY) != std::string::npos) {
+                while (std::getline(inStream, lineCage) && !(lineCage.find(ENDFACILITY) != std::string::npos)) {
+                    if (lineCage.empty()) continue;
+                    else facility_str.push_back(lineCage);
+                }
+            }    
         }
     }
 
+    //cout << cage_str.size() << endl;
     for(int i = 0; i< cage_str.size(); i++) {
-        string token;
-        std::stringstream ss(cage_str[i]);
-        while(std::getline(ss,token,',')) {
-            /**for(j = 0; j < cage_str[i].size(); j++) {
-                cage c;
-            }*/
+        //cout << "HI INI CAGE" << endl;
+        vector<string> x = splits_(cage_str[i],',');
+        cage_temp * temp = new cage_temp(stoi(x[0]), stoi(x[1]), stoi(x[2]), x[3]);
+        //cout << x[0] << " " << x[1] << " " << x[2] << endl;
+        x.clear();
+        //cout << temp->x << " " << temp->y << " " << temp->id << " " << temp->habitat << endl;
+        cageVec.push_back(*temp);
+    }
+
+    //cout << animal_str.size() << endl;
+    for(int i = 0; i< animal_str.size(); i++) {
+        //cout << animal_str[i] << endl;
+        //cout << "HI INI CAGE" << endl;
+        vector<string> x = splits_(animal_str[i],',');
+        bool b = false;
+        if(x[4].compare("True") == 0) {
+            b = true;
         }
+        animal_temp * temp = new animal_temp(stoi(x[0]), stoi(x[1]), x[2], stof(x[3]), b, x[5], x[6]);
+        //cout << x[0] << " " << x[1] << " " << x[2] << endl;
+        x.clear();
+        animalVec.push_back(*temp);
+    }
+    
+    //cout << facility_str.size() << endl;
+    for(int i = 0; i < facility_str.size(); i++) {
+         vector<string> x = splits_(facility_str[i],',');
+         facility_temp * temp = new facility_temp(stoi(x[0]), stoi(x[1]), x[2], x[3]);
+         x.clear();
+         //cout << temp->x << " " << temp->y << " " << temp->type << " " << temp->name << endl;
+         facilityVec.push_back(*temp);
     }
 
-    for(int i = 0; i< facility_str.size(); i++) {
-        //cout << facility_str[i] << endl;
+    for(int i = 0; i < cageVec.size(); i++) {
+        //cout << temp->x << " " << temp->y << " " << temp->name << " " << temp->weight << " " << temp->tamed << " " << temp->habitat << " " << temp->foodtype << endl;
     }
-}
- 
-ConfigStore::ConfigStore(const ConfigStore& C) {
-
-}
-
-ConfigStore& ConfigStore::operator=(const ConfigStore& C) {
-
-    return *this;
 }
--- src/class/ConfigStore/ConfigStore.h
+++ src/class/ConfigStore/ConfigStore.h
@@ -3,24 +3,70 @@
 
 #include <iostream>
 #include <fstream>
-#include <sstream>
+
 #include <map>
 #include <vector>
 #include <string>
-#include <cstring>
-#include <algorithm>
+#include "Util.h"
 using namespace std;
+
+/** @struct cage_temp
+ *  @brief struktur untuk menampung data cage sementara
+ */
+struct cage_temp {
+    cage_temp(int x_=0, int y_=0, int id_=0, string habitat_="") {
+        x = x_;
+        y = y_;
+        id = id_;
+        habitat = habitat_;
+    }
+    int x;
+    int y;
+    int id;
+    string habitat;
+};
+
+/** @struct facility_temp
+ *  @brief struktur untuk menampung data facility sementara
+ */
+struct facility_temp {
+    facility_temp(int x_=0, int y_=0, string type_="", string name_="") {
+        x = x_;
+        y = y_;
+        type = type_;
+        name = name_;
+    }
+    int x;
+    int y;
+    string type;
+    string name;
+};
+
+/** @struct animal_temp
+ *  @brief struktur untuk menampung data animal sementara
+ */
+struct animal_temp {
+    animal_temp(int x_=0, int y_=0, string name_="", float weight_=0.0, bool tamed_= true, string habitat_="", string foodtype_ = "") {
+        x = x_;
+        y = y_;
+        name = name_;
+        weight = weight_;
+        tamed = tamed_;
+        habitat = habitat_;
+        foodtype = foodtype_;
+    }
+    int x;
+    int y;
+    string name;
+    float weight;
+    bool tamed;
+    string habitat;
+    string foodtype;    
+};
 
 /** @class Config
 * Config merupakan kelas loader and parser untuk melakukan pengambilan konfigurasi dari file eksternal
 */
-
-struct cage {
-    int x;
-    int y;
-    int id;
-};
-
 class ConfigStore {
     public:
     /** @brief get
@@ -45,20 +91,20 @@ class ConfigStore {
     private:
     /** @brief Constructor.
     * Melakukan inisialisasi kelas
-    */
+    */  
     ConfigStore(){};
     /** @brief Copy Constructor.
     * Melakukan inisialisasi kelas dengan memasukkan kelas lain sebagai parameternya
     */ 
-    ConfigStore(const ConfigStore&);
+    ConfigStore(const ConfigStore&) {};
     /** @brief Operator =
     * Melakukan inisialisasi kelas dengan operator =
     */ 
-    ConfigStore& operator=(const ConfigStore&);
-    vector<cage> cageVec;
+    ConfigStore& operator=(const ConfigStore&) {};
+    vector<cage_temp> cageVec;
+    vector<animal_temp> animalVec;
+    vector<facility_temp> facilityVec;
     std::map<std::string,std::string> storedConfig;
 };
-
-void removeCharsFromString( string&, char*);
 
 #endif
