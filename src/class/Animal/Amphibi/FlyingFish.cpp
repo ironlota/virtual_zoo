b31fd20c02a6dec53a0b2198002473b6b9f66c6c
aldrichvalentino <aldrich.vh97@gmail.com>
2017-03-13 20:09:29 +0700

    Ditambahin kelas flyingfish dan ostrich okeee

---
+++ src/class/Animal/Amphibi/FlyingFish.cpp
@@ -0,0 +1,27 @@
+#include "FlyingFish.h"
+
+FlyingFish::FlyingFish(int x_, int y_, bool tamed_, float weight_, string foodtype_, string habitat) : 
+				  Amphibi(x_, y_, "FlyingFish", tamed_, weight_, foodtype_, habitat)
+{
+
+}
+
+FlyingFish::FlyingFish(const FlyingFish& H) : Amphibi(H)
+{
+	
+}
+
+FlyingFish::~FlyingFish()
+{
+	//do nothing
+}
+
+FlyingFish& FlyingFish::operator= (const FlyingFish& H)
+{
+	Amphibi::operator=(H);
+}
+
+void FlyingFish::interact()
+{
+	std::cout << "Wushhh" << std::endl;
+}
---
+++ src/class/Animal/Amphibi/FlyingFish.h
@@ -0,0 +1,34 @@
+#include "Amphibi.h"
+
+class FlyingFish : public Amphibi {
+    public:
+        /** @brief Constructor.
+        * Melakukan inisialisasi kelas
+        * @param x_ Posisi X.
+        * @param y_ Posisi Y.
+        * @param tamed_ Menyatakan kejinakan Animal tersebut; false = ganas, true = jinak.
+        * @param weight_ Menyatakan berat Animal tersebut.
+        * @param foodtype_ Menyatakan Animal tersebut Herbivore, Carnivore, atau Omnivore.
+        * @param habitat Menyatakan habitat Animal tersebut.
+        */    
+        FlyingFish(int x_ = 0, int y_ = 0, bool tamed_ = false, float weight_ = 0.5, string foodtype_ = "", string habitat = "");
+        /** @brief Copy Constructor.
+        * Melakukan inisialisasi kelas dengan memasukkan kelas lain sebagai parameternya
+        */ 
+        FlyingFish(const FlyingFish&);
+        /** @brief Destructor.
+        * Dipanggil saat penghancuran objek
+        * @param H FlyingFish lain yang akan dicopy.
+        */
+        ~FlyingFish();
+        /** @brief Operator =
+        * Melakukan inisialisasi kelas dengan operator =
+        * @param H FlyingFish lain yang akan dicopy.
+        * @return Reference to FlyingFish.
+        */ 
+        FlyingFish& operator=(const FlyingFish&);
+        /** @brief Interaksi 
+        */
+        void interact();
+
+};
